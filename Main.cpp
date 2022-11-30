882e9c379fa8fdd9a4462c61209710d3283a43b2
Aditya Kalluri <114735877+Aditya-Kalluri@users.noreply.github.com>
2022-11-30 14:16:57 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,87 +1,226 @@
 #include <bits/stdc++.h>
 #include <fstream>
-using namespace std;
-class doctors{
+#include <sstream>
+class doctor{
     public:
     string Name;
+    string email_id;
+    string PhoneNumber;
+    string TypeOfEmployment;
     string Department;
-    string TypeOfEmployment;
-    long int PhoneNumber;
-    string email_id;
-    doctors* next;
-};
-void NewDoctor(doctors* new_doctor) {
-    new_doctor=new doctors;
-    cout<<"Name: ";
-    cin>>new_doctor->Name;
-    cout<<"Department: ";
-    cin>>new_doctor->Department;
-    cout<<"TypeOfEmployment: ";
-    cin>>new_doctor->TypeOfEmployment;
-    cout<<"Phone Number: ";
-    cin>>new_doctor->PhoneNumber;
-    cout<<"email_id: ";
-    cin>>new_doctor->email_id;
-    new_doctor->next = NULL;
-}
+    string Feedback;
+    string Patients;
+    string Slot1;
+    string Slot2;
+    string Slot3;
+    string Slot4;
+    string Slot5;
+    queue <patient*> q1;
+    queue <patient*> q2;
+    queue <patient*> q3;
+    queue <patient*> q4;
+    queue <patient*> q5;
+    queue <patient*> q6;
+    doctor* next;
 
-
-class patients{
-    public:
-    string Name;
-    string department;
-    string TypeOfEmployment;
-    long int PhoneNumber;
-    string email_id;
-    string address;
-    patients* next;
-};
-void NewPatient(patients* new_patient) {
-    new_patient = new patients;
-    cout<<"Name: ";
-    cin>>new_patient->Name;
-    cout<<"Department: ";
-    cin>>new_patient->department;
-    cout<<"TypeOfEmployment: ";
-    cin>>new_patient->TypeOfEmployment;
-    cout<<"Phone Number: ";
-    cin>>new_patient->PhoneNumber;
-    cout<<"email_id: ";
-    cin>>new_patient->email_id;
-    cout<<"Address: ";
-    cin>>new_patient->address;
-    new_patient->next = NULL;
-}
-void InsertPatient(patients* &patient1, patients* str) {
-    if(patient1 == NULL) {
-        patient1 = str;
-        return;
+    doctor(string Name,string email_id,string PhoneNumber,string TypeOfEmployment,string Department, string Feedback, string Patients, string Slot1, string Slot2, string Slot3, string Slot4, string Slot5) {
+        this->Name = Name;
+        this->Department = Department;
+        this->TypeOfEmployment = TypeOfEmployment;
+        this->PhoneNumber = PhoneNumber;
+        this->email_id = email_id;
+        this->Feedback = Feedback;
+        this->Patients = Patients;
+        this->Slot1 = Slot1;
+        this->Slot2 = Slot2;
+        this->Slot3 = Slot3;
+        this->Slot4 = Slot4;
+        this->Slot5 = Slot5;
+        next = NULL;
     }
-    patients* temp = patient1;
-    while(temp->next!=NULL) {
+};
+bool NameSearchDoc(doctor* &first, string Name) {
+    doctor* temp = first;                  
+    while(temp!=NULL) {
+        if(temp->Name == Name) {
+            return true;
+            break;
+        }
         temp = temp->next;
     }
-    temp->next = str;
+    if(temp == NULL) {
+        return false;
+    }
 }
+doctor* NSDoc(doctor* &first, string Name) {
+    doctor* temp = first;                  
+    while(temp!=NULL) {
+        if(temp->Name == Name) {
+            return temp;
+            break;
+        }
+        temp = temp->next;
+    }
+    if(temp == NULL) {
+        return NULL;
+    }
+}
+void InsertDoctor(doctor* &first, string Name,string email_id,string PhoneNumber,string TypeOfEmployment,string Department, string Feedback, string Patients, string Slot1, string Slot2, string Slot3, string Slot4, string Slot5) {
+    doctor* new_doc = new doctor(Name,email_id, PhoneNumber,TypeOfEmployment,Department, Feedback, Patients, Slot1, Slot2, Slot3, Slot4, Slot5);
+    doctor* temp = first;
+    if(NameSearchDoc(first, Name)==true) {
+        return;
+    }
+
+    if(first==NULL) {
+        first = new_doc;
+    }
+    else {
+        while(temp!=NULL && temp->next != NULL && temp->next->Name.compare(Name)<0) {
+            temp = temp->next;
+        }
+        if(temp == first) {
+            if(temp->Name.compare(Name)>0) {
+            new_doc->next = first;
+            first = new_doc;
+            }
+            else {
+                new_doc->next = first->next;
+                first->next = new_doc;
+            }
+        }
+        else if(temp->next == NULL) {
+            temp->next = new_doc;
+        }
+        else {
+            new_doc->next = temp->next;
+            temp->next = new_doc;
+        }
+    }
+}
+void DeleteDoc(doctor* &doctor1, string Name) {
+    doctor* temp = doctor1;
+
+     if(doctor1->Name == Name) {
+        doctor* remove = doctor1;
+        doctor1 = doctor1->next;
+        delete remove;
+    }                                                  
+    while(temp!=NULL) {
+        if(temp->next->Name == Name) {
+            doctor* dead = temp->next;
+            temp->next = temp->next->next;
+            delete dead;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+void SearchByDeptDoc(doctor* doctor1, string Department){
+    doctor* temp = doctor1;
+    if(doctor1 == NULL) {
+       return;
+    }
+    while(temp!=NULL) {
+       if(temp->Department == Department)
+       cout<<temp->Name<<"  "<<temp->PhoneNumber<<"  " << temp->Feedback<<"  "<<temp->Patients<< endl;
+       temp = temp->next;
+    }      
+}
+void SearchByNameDoc(doctor* doctor1, string Name) {
+    doctor* temp = doctor1;
+    if(doctor1==NULL) {
+        return;
+    }
+    while(temp!=NULL) {
+        if(temp->Name == Name) {
+            cout<<temp->Name<<"  "<<temp->Department<<"  "<<temp->TypeOfEmployment<<"  "<<temp->PhoneNumber<<"  "<<temp->email_id<<"  "<<temp->Feedback<<"  "<<temp->Patients<< endl;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+
+void SearchByPhoneNoDoc(doctor* doctor1, string PhoneNumber) {
+    doctor* temp = doctor1;
+    if(doctor1==NULL) {
+        return;
+    }
+    while(temp!=NULL) {
+        if(temp->PhoneNumber == PhoneNumber) {
+            cout<<temp->Name<<"  "<<temp->Department<<"  "<<temp->TypeOfEmployment<<"  "<<temp->PhoneNumber<<"  "<<temp->email_id<<"  "<<temp->Feedback<<"  "<<temp->Patients<< endl;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+void displayDocs(doctor* doctor1) {
+    if(doctor1 == NULL) {
+        return;
+    }
+    doctor* temp = doctor1;
+    while(temp!=NULL) {
+        cout<<temp->Name<<"  "<<temp->Department<<"  "<<temp->TypeOfEmployment<<"  "<<temp->PhoneNumber<<"  "<<temp->email_id<<"  "<<temp->Feedback<<"  "<<temp->Patients<< endl;
+        temp = temp->next;
+    }
+}
+bool link(doctor* doctor1, string DocName, string PatName, string Slot) {
+    doctor* Doc = NSDoc(doctor1, DocName);
+    queue<patient*> q; 
+    if(Slot == "1")  q = Doc->q1;
+    if(Slot == "2")  q = Doc->q2;
+    if(Slot == "3")  q = Doc->q3;
+    if(Slot == "4")  q = Doc->q4;
+    if(Slot == "5")  q = Doc->q5;
+    if(Slot == "6")  q = Doc->q6;
+    while(!q.empty() and !(q.front()->Name==PatName)) {
+        q.pop();
+    }
+    if(q.empty())  return false;
+    else  return true;
+}
+
 int main() {
-    doctors* doctor1 = NULL;
-    doctors* str;
-    NewDoctor(str);
-    patients* patient1 = NULL;
-    patients* str2;
-    NewPatient(str2);
-    fstream MyFile;
-    MyFile.open("Doctors.txt", ios::out); //write 
-    if(MyFile.is_open()) {
-         MyFile << "Name: "<<str->Name<<"\n";
-         MyFile << "Department: "<<str->Department<<"\n";
-         MyFile << "Type Of Employment: "<<str->TypeOfEmployment<<"\n";
-         MyFile.close();
-     }
-     MyFile.open("Patients.txt", ios::out);
-     if(MyFile.is_open()) {
-           MyFile << "Name: "<<str2->Name<<"\n";
-         MyFile << "Department: "<<str2->department<<"\n";
-         MyFile << "Type Of Employment: "<<str2->TypeOfEmployment<<"\n";
-     }
+    doctor* doctor1 = NULL;
+    ifstream inputFile;
+    inputFile.open("BM2043_PROJECT_DATA_1.csv");
+
+    string line="";
+    getline(inputFile, line);
+    line="";
+    while(getline(inputFile, line))
+    {
+       string Name;
+       string Department;
+       string TypeOfEmployment;
+       string PhoneNumber;
+       string Feedback;
+       string email_id;
+       string Patients;
+       string Slot1;
+       string Slot2;
+       string Slot3;
+       string Slot4;
+       string Slot5;
+
+       stringstream inputString(line);
+       getline(inputString, Name, ',');
+       getline(inputString, email_id, ',');
+       getline(inputString, PhoneNumber, ',');
+       getline(inputString, TypeOfEmployment, ',');
+       getline(inputString, Department, ',');
+       getline(inputString, Feedback, ',');
+       getline(inputString, Patients, ',');
+       getline(inputString, Slot1, ',');
+       getline(inputString, Slot2, ',');
+       getline(inputString, Slot3, ',');
+       getline(inputString, Slot4, ',');
+       getline(inputString, Slot5, ',');
+
+       line="";
+
+      if(!NameSearchDoc(doctor1,Name) && TypeOfEmployment =="DOCTOR" ) {
+      InsertDoctor(doctor1,Name,email_id,PhoneNumber,TypeOfEmployment,Department,Feedback,Patients,Slot1,Slot2,Slot3,Slot4,Slot5);
+      }
+    }
 }
