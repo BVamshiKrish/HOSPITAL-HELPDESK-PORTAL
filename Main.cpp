faee05c3af6fb57bba17763ddc8b4eb74bc5836b
ShakthiAshika <114737457+ShakthiAshika@users.noreply.github.com>
2022-11-30 14:27:34 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,164 @@
 #include <bits/stdc++.h>
 #include <fstream>
 #include <sstream>
+using namespace std;
+
+class patient{
+    public:
+    string Name;
+    string email_id;
+    string PhoneNumber;
+    string Designation;
+    string Department;
+    string Roll_no;
+    string Ethics_rating;
+    string Address;
+    patient* next;
+
+    patient(string Name,string email_id,string PhoneNumber,string Department,string Roll_no,string Ethics_rating,string Address,string Designation) {
+        this->Name = Name;
+        this->Department = Department;
+        this->PhoneNumber = PhoneNumber;
+        this->email_id = email_id;
+        this->Roll_no = Roll_no;
+        this->Ethics_rating = Ethics_rating;
+        this->Address = Address;
+        this->Designation = Designation;
+        next = NULL;
+    }
+};
+
+bool Namesearch(patient* &first, string Name) {
+    patient* temp = first;                  
+    while(temp!=NULL) {
+        if(temp->Name == Name) {
+            return true;
+            break;
+        }
+        temp = temp->next;
+    }
+    if(temp == NULL) {
+        return false;
+    }
+}
+
+patient* Ns(patient* &first, string Name) {
+    patient* temp = first;                  
+    while(temp!=NULL) {
+        if(temp->Name == Name) {
+            return temp;
+            break;
+        }
+        temp = temp->next;
+    }
+    if(temp == NULL) {
+        return NULL;
+    }
+}
+
+void InsertPatient(patient* &first, string Name,string Designation ,string Department ,string email_id,string Roll_no,string PhoneNumber,string Ethics_rating,string Address) {
+    patient* new_pat = new patient(Name,Designation,Department,email_id,Roll_no,PhoneNumber,Ethics_rating,Address);
+    patient* temp = first;
+    if(Namesearch(first, Name)==true) {
+        return;
+    }
+
+    if(first==NULL) {
+        first = new_pat;
+    }
+    else {
+        while(temp!=NULL && temp->next != NULL && temp->next->Name.compare(Name)<0) {
+            temp = temp->next;
+        }
+        if(temp == first) {
+            if(temp->Name.compare(Name)>0) {
+            new_pat->next = first;
+            first = new_pat;
+            }
+            else {
+                new_pat->next = first->next;
+                first->next = new_pat;
+            }
+        }
+        else if(temp->next == NULL) {
+            temp->next = new_pat;
+        }
+        else {
+            new_pat->next = temp->next;
+            temp->next = new_pat;
+        }
+    }
+}
+
+void DeletePat(patient* &patient1, string Name) {
+    patient* temp = patient1;
+
+     if(patient1->Name == Name) {
+        patient* remove = patient1;
+        patient1 = patient1->next;
+        delete remove;
+    }                                                  
+    while(temp!=NULL) {
+        if(temp->next->Name == Name) {
+            patient* dead = temp->next;
+            temp->next = temp->next->next;
+            delete dead;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+
+void SearchByname(patient* patient1, string Name) {
+    patient* temp = patient1;
+    if(patient1==NULL) {
+        return;
+    }
+    while(temp!=NULL) {
+        if(temp->Name == Name) {
+            cout<<temp->Name<<"  "<<temp->Department<<"  "<<"  "<<temp->PhoneNumber<<"  "<<temp->email_id<<"  "<<temp->Roll_no<<"  "<<temp->Ethics_rating<<"  "<<temp->Address<<"  "<<temp->Designation<< endl;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+
+void SearchByPhoneno(patient* patient1, string PhoneNumber) {
+    patient* temp = patient1;
+    if(patient1==NULL) {
+        return;
+    }
+    while(temp!=NULL) {
+        if(temp->PhoneNumber == PhoneNumber) {
+            cout<<temp->Name<<"  "<<"  "<<temp->Department<<"  "<<temp->PhoneNumber<<"  "<<temp->email_id<<"  "<<temp->Roll_no<<"  "<<temp->Ethics_rating<<"  " <<temp->Address<<"  "<<temp->Designation <<endl;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+void UpdatePat(patient* patient1, string Name, string email_id, string PhoneNumber,string Department,string Roll_no,string Ethics_rating,string Address,string Designation) {
+    patient* Pat = Ns(patient1, Name);
+    Pat->email_id = email_id;
+    Pat->PhoneNumber = PhoneNumber;
+    Pat->Department = Department;
+    Pat->Designation = Designation;
+    Pat->Roll_no = Roll_no;
+    Pat->Ethics_rating = Ethics_rating;
+    Pat->Address = Address;
+
+}
+
+void displayPats(patient* patient1) {
+    if(patient1 == NULL) {
+        return;
+    }
+    patient* temp = patient1;
+    while(temp!=NULL) {
+        cout<<temp->Name<<"  "<<temp->Department<<"  "<<"  "<<temp->PhoneNumber<<"  "<<temp->email_id<<"  "<<temp->Department<<"  "<<temp->Roll_no<<"  "<<temp->Ethics_rating<<"  "<<temp->Address<<"  "<<temp->Designation <<endl;
+        temp = temp->next;
+    }
+}
+
 class doctor{
     public:
     string Name;
@@ -178,6 +336,36 @@ bool link(doctor* doctor1, string DocName, string PatName, string Slot) {
     }
     if(q.empty())  return false;
     else  return true;
+}
+
+void feedback(doctor* &doctor1,string DocName,int x){
+
+    doctor* doc = NS(doctor1, DocName);
+
+    string feedback,patients,emailid,phoneNumber,typeOfEmployment,department,slot1,slot2,slot3,slot4,slot5 ;
+    doc->Feedback=feedback;
+    doc->Patients=patients;
+    doc->email_id = emailid;
+    doc->PhoneNumber = phoneNumber;
+    doc->TypeOfEmployment = typeOfEmployment;
+    doc->Department = department;
+    doc->Slot1 = slot1;
+    doc->Slot2 = slot2;
+    doc->Slot3 = slot3;
+    doc->Slot4 = slot4;
+    doc->Slot5 = slot5;
+    
+    int feedback1 = stoi(feedback);
+    int patients1 = stoi(patients);
+  
+    feedback1=((feedback1*patients1)+x)/(patients1+1);
+    patients1++;
+
+    feedback = to_string(feedback1);
+    patients = to_string(patients1);
+    UpdateDoc(doctor1, DocName,emailid,phoneNumber,typeOfEmployment,department, feedback,patients,slot1,slot2,slot3,slot4,slot5);
+
+    SearchByName(doctor1,DocName);
 }
 
 int main() {
